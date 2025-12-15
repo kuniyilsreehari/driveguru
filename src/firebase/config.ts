e3d8dd0de88c5839017ac55ae7367ba9c71fa22e
Acet live <acetlive@gmail.com>
2025-12-15 16:15:35 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8621980584-11b8b",
+  "appId": "1:14947593216:web:fde8982079c3b45961fce5",
+  "apiKey": "AIzaSyALVoTRdOuqfrKosZ-Nnyd8lfMz5W8T9ew",
+  "authDomain": "studio-8621980584-11b8b.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "14947593216"
+};
