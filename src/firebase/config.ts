36eebfe38a5c4c76b02bbf203fb9f525f5a2c121
Acet live <acetlive@gmail.com>
2025-12-20 17:17:05 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,10 +1,11 @@
 
 export const firebaseConfig = {
-  "projectId": "studio-8621980584-11b8b",
-  "appId": "1:14947593216:web:fde8982079c3b45961fce5",
-  "apiKey": "AIzaSyALVoTRdOuqfrKosZ-Nnyd8lfMz5W8T9ew",
-  "authDomain": "studio-8621980584-11b8b.firebaseapp.com",
-  "storageBucket": "studio-8621980584-11b8b.appspot.com",
-  "measurementId": "",
-  "messagingSenderId": "14947593216"
+  apiKey: "AIzaSyALVoTRdOuqfrKosZ-Nnyd8lfMz5W8T9ew",
+  authDomain: "studio-8621980584-11b8b.firebaseapp.com",
+  projectId: "studio-8621980584-11b8b",
+  storageBucket: "studio-8621980584-11b8b.firebasestorage.app",
+  messagingSenderId: "14947593216",
+  appId: "1:14947593216:web:fde8982079c3b45961fce5",
+  measurementId: ""
 };
+
